109b50ba13b66a777ede425d9ce1870e464688e1
tsteer <trevor.piano@gmail.com>
2019-06-21 16:31:10 -0600

    Update Aptfile

--- src/Sequencer.js
+++ src/Sequencer.js
@@ -219,7 +219,7 @@ class Sequencer extends React.Component {
     let audioTrack = this.audioDestination.stream.getAudioTracks()[0];
 
     let combined = new MediaStream([videoTrack, audioTrack]);
-    var options = { mimeType: 'video/mp4' }; 
+    var options = { mimeType: 'video/webm' }; 
     let recorder = new MediaRecorder(combined, options);
 
     recorder.ondataavailable = (e) => {
@@ -228,7 +228,7 @@ class Sequencer extends React.Component {
 
     recorder.onstop = () => {
       if (this.chunks.length) {
-        this.blob = new Blob(this.chunks, { type: 'video/mp4' });
+        this.blob = new Blob(this.chunks, { type: 'video/webm' });
         this.changeInstructionNumber(6);
       }
     };
